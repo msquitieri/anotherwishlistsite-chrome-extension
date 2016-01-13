698ddbe47c2c852e58426217621b1694f9695f1e
Michael Squitieri <michael.squitieri@gmail.com>
2016-01-12 23:49:28 -0500

    update whitespace and remove unused .gitkeep file

--- app/models/url.js
+++ app/models/url.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	url: DS.attr('string'),
-	title: DS.attr('string'),
-	images: DS.attr()
+  url   : DS.attr('string'),
+  title : DS.attr('string'),
+  images: DS.attr()
 });
